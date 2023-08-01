d053bf3f56a2494bd1925276f60db2853be05d0f
unknown <humzaliaqat01@gmail.com>
2023-08-01 11:49:04 +0500

    hosted website on github

---
+++ js/app.js
@@ -0,0 +1,64 @@
+$(document).ready(function () {
+    let $btns = $('.project-area .button-group button');
+
+    $btns.click(function (e) {
+        $('.project-area .button-group button').removeClass('active');
+        e.target.classList.add('active');
+
+
+        let selector = $(e.target).attr("data-filter");
+        $('.project-area .grid').isotope({
+            filter: selector
+        });
+
+        return false
+
+    });
+
+    $('.project-area .button-group .btn1').trigger('click');
+
+    // Pop Up Image
+
+    $('.project-area .grid .test-popup-link').magnificPopup({
+        type: 'image',
+        gallery: { enabled: true }
+
+        // other options
+    })
+
+    // Owl 
+    $('.site-main .about-area .owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            544: {
+                items: 2
+            }
+        }
+    });
+
+    // Sticky Navigation
+
+    let nav_offset_top = $('.header_area').height() + 50;
+
+    function navbarFixed() {
+        if ($('.header_area').length) {
+            $(window).scroll(function () {
+                let scroll = $(window).scrollTop();
+                if (scroll >= nav_offset_top) {
+                    $('.header_area .main_menu').addClass('navbar_fixed');
+                }
+                else {
+                    $('.header_area .main_menu').removeClass('navbar_fixed');
+                }
+            })
+        }
+    }
+
+    navbarFixed();
+
+});
